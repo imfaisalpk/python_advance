a8ea05acb0b4c5065c385f10aa65a0b41930723a
M Faisal <muhammadfaisal9t3@outlook.com>
2019-01-03 18:47:01 +0500

    Keywords and Identifier, Statements & Comments, Python Variables Done

---
+++ main.py
@@ -0,0 +1,34 @@
+
+def test_func():
+    '''i am a test func'''
+    print("Hello I am Faisal")
+
+
+# print(test_func.__doc__)
+# test_func()
+
+if __name__ == "__main__":
+
+    list_ex = [1,2,3,4]
+    tuple_ex = (1,2,3)
+    dict_ex = {'1':1,'2':2,'3':3}
+    set_ex = {1,2,3,3}
+
+    print(set_ex)
+
+    # a = 1
+    # a="1"
+    # a = 1.0
+    # a = '1'
+    # a =100
+    # b= 0b01100100
+    # o = 0o144
+    # h = 0x64
+    # i = 3.14j
+    # f = 3.14
+    # unicode = u"\u00dcnic\u00f6de"
+    # raw_str = r'abc\n'
+    # print(unicode)
+    # print(raw_str)
+
+    # print(a,b,o,h,f,i,i.real,i.imag)
